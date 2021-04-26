2bda8d244b78ab8dcd307034d9d9a67e1b02d1fd
markm <mark@home>
2021-04-27 08:04:42 +1000

    base64-encode

--- main.go
+++ main.go
@@ -1,73 +1,18 @@
 package main
 
 import (
-	//"encoding/json"
-	//"fmt"
-	//"go/ast"
-	//"log"
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-type person struct {
-	First string `json:"firstName"`
-}
-
 func main() {
-	//p1 := person{
-	//	First: "Matthew",
-	//}
-	//
-	//fmt.Println("Person one name ", p1.First)
-	//
-	//p2 := person{
-	//	First: "Mark",
-	//}
-	//
-	//persons := []person{p1, p2}
-	//
-	//personsJson, err := json.Marshal(persons)
-	//if err != nil {
-	//	log.Panic(err) //panic on a programmer error
-	//}
-	//fmt.Println("JSON of person ... ", string(personsJson))
-	//
-	//persons2 := []person{}
-	//
-	//err = json.Unmarshal(personsJson, &persons2)
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-	//fmt.Println("Converted back to person struct ", persons2)
 
-	http.HandleFunc("/encode", encodeHandler)
-	http.HandleFunc("/decode", decodeHandler)
-	http.ListenAndServe(":9000", nil)
-}
+	toString := base64.StdEncoding.EncodeToString([]byte("usermark:password123"))
+	fmt.Println(toString)
 
-func encodeHandler(w http.ResponseWriter, r *http.Request) {
-
-	p1 := person{
-		First: "Matthew",
-	}
-
-	err := json.NewEncoder(w).Encode(p1)
+	_, err := base64.StdEncoding.DecodeString(toString)
 	if err != nil {
-		log.Println("Encoding was bad", err)
+		fmt.Errorf("This could not be decoded %s %w ", toString, err)
 	}
-
-}
-
-func decodeHandler(w http.ResponseWriter, r *http.Request) {
-
-	p1 := person{}
-	err := json.NewDecoder(r.Body).Decode(&p1)
-	if err != nil {
-		return
-	}
-	fmt.Println("Decode object", p1)
-
 
 }
