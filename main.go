78f3e5bbc241740f93d756a31f050704246a38b5
markm <mark@home>
2021-04-26 18:36:02 +1000

    included unmarshall code

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type person struct {
-	First string
+	First string  `json:"firstName"`
 }
 
 func main() {
@@ -15,16 +15,27 @@ func main() {
 		First: "Matthew",
 	}
 
+	fmt.Println("Person one name ", p1.First)
+
 	p2 := person{
 		First: "Mark",
 	}
 
 	persons := []person{p1, p2}
 
-	bs, err := json.Marshal(persons)
+	personsJson, err := json.Marshal(persons)
 	if err != nil {
 		log.Panic(err)  //panic on a programmer error
 	}
-	fmt.Println(string(bs))
+	fmt.Println("JSON of person ... ", string(personsJson))
+
+	persons2 := []person{}
+
+	err = json.Unmarshal(personsJson, &persons2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("Converted back to person struct ", persons2)
+
 
 }
