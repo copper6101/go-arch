b28949676669bcb7ea6b88062f62b609f7cc9075
markm <mark@home>
2021-04-27 07:41:04 +1000

    http encode/decode of json

--- main.go
+++ main.go
@@ -1,41 +1,73 @@
 package main
 
 import (
+	//"encoding/json"
+	//"fmt"
+	//"go/ast"
+	//"log"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
-	First string  `json:"firstName"`
+	First string `json:"firstName"`
 }
 
 func main() {
+	//p1 := person{
+	//	First: "Matthew",
+	//}
+	//
+	//fmt.Println("Person one name ", p1.First)
+	//
+	//p2 := person{
+	//	First: "Mark",
+	//}
+	//
+	//persons := []person{p1, p2}
+	//
+	//personsJson, err := json.Marshal(persons)
+	//if err != nil {
+	//	log.Panic(err) //panic on a programmer error
+	//}
+	//fmt.Println("JSON of person ... ", string(personsJson))
+	//
+	//persons2 := []person{}
+	//
+	//err = json.Unmarshal(personsJson, &persons2)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//fmt.Println("Converted back to person struct ", persons2)
+
+	http.HandleFunc("/encode", encodeHandler)
+	http.HandleFunc("/decode", decodeHandler)
+	http.ListenAndServe(":9000", nil)
+}
+
+func encodeHandler(w http.ResponseWriter, r *http.Request) {
+
 	p1 := person{
 		First: "Matthew",
 	}
 
-	fmt.Println("Person one name ", p1.First)
-
-	p2 := person{
-		First: "Mark",
-	}
-
-	persons := []person{p1, p2}
-
-	personsJson, err := json.Marshal(persons)
+	err := json.NewEncoder(w).Encode(p1)
 	if err != nil {
-		log.Panic(err)  //panic on a programmer error
+		log.Println("Encoding was bad", err)
 	}
-	fmt.Println("JSON of person ... ", string(personsJson))
 
-	persons2 := []person{}
+}
 
-	err = json.Unmarshal(personsJson, &persons2)
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+
+	p1 := person{}
+	err := json.NewDecoder(r.Body).Decode(&p1)
 	if err != nil {
-		log.Panic(err)
+		return
 	}
-	fmt.Println("Converted back to person struct ", persons2)
+	fmt.Println("Decode object", p1)
 
 
 }
