f5695db6a2c8af7a9ffe7ec2bddeebaa27b07bee
markm <mark@home>
2021-04-28 17:10:12 +1000

    hmac code

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"encoding/base64"
 	"fmt"
 	"log"
@@ -12,7 +14,14 @@ type person struct {
 	First string `json:"firstName"`
 }
 
+var key = []byte{}
+
+
 func main() {
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
+
 	var secret string
 	secret = base64.StdEncoding.EncodeToString([]byte("userName:password123"))
 	fmt.Println(secret)
@@ -23,13 +32,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("password hash of %s is %s\n", passwordTest, passwordHash)
+	fmt.Printf("password hash of %v is %v\n", passwordTest, passwordHash)
 
 	err = comparePassword(passwordTest, passwordHash)
 	if err != nil {
 		log.Fatalln("password does not match")
 	}
 	fmt.Println("Great, passwords match")
+
+	signedMessaged, _ := signMessage([]byte(passwordTest))
+
+	fmt.Println(signedMessaged)
+
+	same, _ := checkSignMessageIsSame(passwordTest, signedMessaged)
+
+	fmt.Printf("Same signature %v\n", same)
+
 }
 
 func hashPassword(password string) ([]byte, error) {
@@ -46,4 +64,24 @@ func comparePassword(password string, hashPassword []byte) error {
 		return fmt.Errorf("error comparing password: %w", err)
 	}
 	return nil
+}
+
+func signMessage(message []byte) ([]byte, error) {
+
+	hash := hmac.New(sha512.New, key)
+
+	_, err := hash.Write(message)
+	if err != nil {
+		return nil, fmt.Errorf("error while hashing %w", err)
+	}
+
+	signature := hash.Sum(nil)
+	return signature, nil
+}
+
+func checkSignMessageIsSame(message string, signature []byte) (bool, error) {
+
+	newSig, _ := signMessage([]byte(message))
+	same := hmac.Equal(newSig, signature)
+	return same, nil
 }
