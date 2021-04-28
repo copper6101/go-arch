162e577a69fe273f4e51ca588360cd07c1de535b
markm <mark@home>
2021-04-28 10:49:39 +1000

    http-basic-auth

--- main.go
+++ main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
-	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 )
 
+type person struct {
+	First string `json:"firstName"`
+}
+/**
+curl -XGET http://localhost:9000/encode
+
+curl -XPOST http://localhost:9000/decode --data '{"firstName":"Bob"}' -H "Content-Type:application/json"
+curl -XPOST http://localhost:9000/decode --data '{"firstName":"Bob"}' -H "Content-Type:application/json" --user markm:password123
+
+*/
 func main() {
 
-	toString := base64.StdEncoding.EncodeToString([]byte("usermark:password123"))
-	fmt.Println(toString)
+	http.HandleFunc("/encode", encodeHandler)
+	http.HandleFunc("/decode", decodeHandler)
+	http.ListenAndServe(":9000", nil)
+}
 
-	_, err := base64.StdEncoding.DecodeString(toString)
-	if err != nil {
-		fmt.Errorf("This could not be decoded %s %w ", toString, err)
+func encodeHandler(w http.ResponseWriter, r *http.Request) {
+
+	p1 := person{
+		First: "Matthew",
 	}
 
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encoding was bad", err)
+	}
+
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+
+	p1 := person{}
+	err := json.NewDecoder(r.Body).Decode(&p1)
+	if err != nil {
+		return
+	}
+	fmt.Println("Decode object", p1)
+
+	auth, password, ok := r.BasicAuth()
+
+	if ok {
+		fmt.Printf("Basic Auth data is user %s & password %s", auth, password)
+	} else {
+		fmt.Println("No Basic Auth found in the request")
+	}
 }
